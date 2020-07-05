01bb93170eff7677ac0e74bb56ccf11ac1aad90d
Henry Seed <henry@jaipuna.com>
2020-07-05 14:12:30 +1200

    adds stops to timer score buttons

--- sliderControl.js
+++ sliderControl.js
@@ -1,20 +1,31 @@
 function changeRoundLength(amount) {
     timeTotal += amount;
-    if (timeTotal < 5) {
+    if (timeTotal <= 5) {
         timeTotal = 5;
-    } else if (timeTotal > 180) {
+        document.getElementById("roundTimeDown").disabled = true;
+    } else if (timeTotal >= 180) {
         timeTotal = 180;
+        document.getElementById("roundTimeUp").disabled = true;
+    } else {
+        document.getElementById("roundTimeDown").disabled = false;
+        document.getElementById("roundTimeUp").disabled = false;
     }
     timeLeft = timeTotal;
     document.getElementById("roundTimeTotalShower").innerHTML = timeTotal;
     document.getElementById("roundInputPreview").innerHTML = timeTotal;
 }
+
 function changeWinPoint(amount) {
     scoreTotal += amount;
-    if (scoreTotal < 10) {
+    if (scoreTotal <= 10) {
         scoreTotal = 10;
-    } else if (scoreTotal > 100) {
+        document.getElementById("winningScoreDown").disabled = true;
+    } else if (scoreTotal >= 100) {
         scoreTotal = 100;
+        document.getElementById("winningScoreUp").disabled = true;
+    } else {
+        document.getElementById("winningScoreDown").disabled = false;
+        document.getElementById("winningScoreUp").disabled = false;
     }
     document.getElementById("gameInputPreview").innerHTML = scoreTotal;
     document.getElementById("winningPointTotalShower").innerHTML = scoreTotal;
